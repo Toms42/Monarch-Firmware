8fb38e51f5d442e439e5eba211e307e1319f1b54
Absolute0K <AbsoluteA0K@gmail.com>
2018-12-29 21:52:13 +0100

    Added board.h and IR LED blink test

---
+++ board.h
@@ -0,0 +1,34 @@
+/*
+ * board.h
+ *
+ *  Created on: Dec 22, 2018
+ *      Author: Jae Choi
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+
+#define FUNCTION_PWM_LED        GPIO_SECONDARY_MODULE_FUNCTION
+#define PORT_IR_LED             GPIO_PORT_P1
+#define PORT_PWM_LED            GPIO_PORT_P1
+#define PIN_IR_LED              GPIO_PIN0
+#define PIN_PWM_LED             GPIO_PIN1
+
+#define FUNCTION_PWM_SERVO      GPIO_SECONDARY_MODULE_FUNCTION
+#define PORT_RIGHT_SERVO        GPIO_PORT_P1
+#define PORT_LEFT_SERVO         GPIO_PORT_P1
+#define PIN_RIGHT_SERVO         GPIO_PIN4
+#define PIN_LEFT_SERVO          GPIO_PIN5
+
+#define FUNCTION_I2C            GPIO_PRIMARY_MODULE_FUNCTION
+#define PORT_I2C_SCL            GPIO_PORT_P1
+#define PORT_I2C_SDA            GPIO_PORT_P1
+#define PIN_I2C_SCL             GPIO_PIN3
+#define PIN_I2C_SDA             GPIO_PIN2
+
+#define GPIO_PORT_VBATT         GPIO_PORT_P1
+#define GPIO_PIN_VBATT          GPIO_PIN7
+
+
+#endif /* BOARD_H_ */
--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include "driverlib.h"
+#include "board.h"
 
-int main(void) {
+void clock_init(uint32_t MCLK_desired_khz);
 
+int main(void)
+{
+    uint32_t i;
+
+    /* Stop the Watchdog timer - Why? */
     WDT_A_hold(WDT_A_BASE);
 
+    /* Calibrate and set up MCLK as 16MHz */
+    clock_init(16000);
+
+    // Set P1.0/IR LED pin to output direction
+    GPIO_setAsOutputPin(PORT_IR_LED, PIN_IR_LED);
+
+    while(1)
+    {
+        // Toggle P1.0 output
+        GPIO_toggleOutputOnPin(PORT_IR_LED, PIN_IR_LED);
+
+        // Delay
+        for(i = 100000; i > 0; i--);
+    }
     return (0);
+}
+
+void clock_init(uint32_t MCLK_desired_khz)
+{
+    uint16_t ratio = ((uint32_t) (MCLK_desired_khz * 1000) / 32768);
+    //Set DCO FLL reference = REFO
+    CS_initClockSignal(CS_FLLREF, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    //Set ACLK = REFO
+    CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    //Create struct variable to store proper software trim values
+    CS_initFLLParam param = {0};
+    //Set Ratio/Desired MCLK Frequency, initialize DCO, save trim values
+    CS_initFLLCalculateTrim(MCLK_desired_khz, ratio, &param);
+    //Clear all OSC fault flag
+    CS_clearAllOscFlagsWithTimeout(1000);
+    //For demonstration purpose, change DCO clock freq to 16MHz
+    CS_initFLLSettle(16000, 488);
+    //Clear all OSC fault flag
+    CS_clearAllOscFlagsWithTimeout(1000);
+    //Reload DCO trim values that were calculated earlier
+    CS_initFLLLoadTrim(MCLK_desired_khz, ratio, &param);
+    //Clear all OSC fault flag
+    CS_clearAllOscFlagsWithTimeout(1000);
+    //Enable oscillator fault interrupt
+    // SFR_enableInterrupt(SFR_OSCILLATOR_FAULT_INTERRUPT);
+    // Enable global interrupt
+    // __bis_SR_register(GIE);
+    //Verify if the Clock settings are as expected
+    uint32_t clockValue = CS_getSMCLK();
+             clockValue = CS_getMCLK();
+             clockValue = CS_getACLK();
+
+    // Configure one FRAM waitstate as required by the device datasheet for MCLK
+    // operation beyond 8MHz _before_ configuring the clock system. (PAGE 16)
+    FRCTL0 = FRCTLPW | NWAITS_1;
+
 }
